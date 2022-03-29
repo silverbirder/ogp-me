44f16d59f88122209032bd67f32ec9376c232754
shibamoto <silverbirder@gmail.com>
2022-03-29 22:48:50 +0900

    Process loading and started html

--- ogp-me.js
+++ ogp-me.js
@@ -1,16 +1,33 @@
 (() => {
-  const template = document.createElement("template");
-  template.innerHTML = `
-  <div>hello world</div>
-    `;
+  const containerTemplate = document.createElement("template");
+  containerTemplate.innerHTML = `<div id="container"></div>`;
+  const loadingTemplate = document.createElement("template");
+  loadingTemplate.innerHTML = `<div id="loading">loading...</div>`;
+  const startTemplate = document.createElement("template");
+  startTemplate.innerHTML = `<div id="started">started!</div>`;
+
   window.customElements.define(
     "ogp-me",
     class extends HTMLElement {
       constructor() {
         super();
         this.attachShadow({ mode: "open" });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.appendChild(containerTemplate.content.cloneNode(true));
       }
+      connectedCallback() {
+        this._render();
+      }
+      _render() {
+        this.shadowRoot
+          .querySelector("#container")
+          .appendChild(loadingTemplate.content.cloneNode(true));
+        setTimeout(() => {
+          const parent = this.shadowRoot.querySelector("#container");
+          const loading = this.shadowRoot.querySelector("#loading");
+          parent.replaceChild(startTemplate.content.cloneNode(true), loading);
+        }, 500);
+      }
+      _fetch() {}
     }
   );
 })();
