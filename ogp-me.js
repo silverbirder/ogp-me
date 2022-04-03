a39f949a0fe4c471741ba339c9b7f60129bdc5b7
shibamoto <silverbirder@gmail.com>
2022-04-03 21:49:38 +0900

    :sparkles: Introduce fetch url and render card ui

--- ogp-me.js
+++ ogp-me.js
@@ -1,10 +1,46 @@
 (() => {
-  const containerTemplate = document.createElement("template");
-  containerTemplate.innerHTML = `<div id="container"></div>`;
-  const loadingTemplate = document.createElement("template");
-  loadingTemplate.innerHTML = `<div id="loading">loading...</div>`;
-  const startTemplate = document.createElement("template");
-  startTemplate.innerHTML = `<div id="started">started!</div>`;
+  const template = document.createElement("template");
+  template.innerHTML = `
+  <style>
+      .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        width: 512px;
+      }
+      .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+      }
+      .container {
+        padding: 2px 16px;
+        background-color: #f2f3f5;
+      }
+      .img {
+        width: 100%;
+        height: auto;
+      }
+      .site {
+        color: #606770;
+        font-size: 12px;
+        text-transform: uppercase;
+      }
+      .title {
+        font-weight: 600;
+      }
+      .description {
+        color: #606770;
+        font-size: 14px;
+        line-height: 20px;
+      }
+  </style>
+  <div class="card">
+    <a class="link" href="#" target="_blank"><img class="img" /></a>
+    <div class="container">
+      <p class="site"></p>
+      <p class="title"></p>
+      <p class="description"></p>
+    </div>
+  </div>
+  `;
 
   window.customElements.define(
     "ogp-me",
@@ -12,29 +48,39 @@
       constructor() {
         super();
         this.attachShadow({ mode: "open" });
-        this.shadowRoot.appendChild(containerTemplate.content.cloneNode(true));
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
       }
       connectedCallback() {
         this._render();
       }
-      async _render() {
+      _render() {
         const proxy = this.getAttribute("proxy");
         const src = this.getAttribute("src");
-        const res = await (await fetch(`${proxy}/${src}`)).text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(res, "text/html");
-        console.log(doc.querySelector("title").innerText);
-
-        this.shadowRoot
-          .querySelector("#container")
-          .appendChild(loadingTemplate.content.cloneNode(true));
-        setTimeout(() => {
-          const parent = this.shadowRoot.querySelector("#container");
-          const loading = this.shadowRoot.querySelector("#loading");
-          parent.replaceChild(startTemplate.content.cloneNode(true), loading);
-        }, 500);
+        fetch(`${proxy}/${src}`).then((res) => {
+          res.text().then((txt) => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(txt, "text/html");
+            const image = doc
+              .querySelector("meta[property='og:image']")
+              .getAttribute("content");
+            const site = doc
+              .querySelector("meta[property='og:site_name']")
+              .getAttribute("content");
+            const title = doc
+              .querySelector("meta[property='og:title']")
+              .getAttribute("content");
+            const description = doc
+              .querySelector("meta[property='og:description']")
+              .getAttribute("content");
+            this.shadowRoot.querySelector(".img").setAttribute("src", image);
+            this.shadowRoot.querySelector(".site").innerHTML = site;
+            this.shadowRoot.querySelector(".title").innerHTML = title;
+            this.shadowRoot.querySelector(".description").innerHTML =
+              description;
+            this.shadowRoot.querySelector(".link").setAttribute("href", src);
+          });
+        });
       }
-      _fetch() {}
     }
   );
 })();
