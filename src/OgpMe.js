1962040bec4058738e7f300f01c006f527440c6d
shibamoto <silverbirder@gmail.com>
2022-04-05 21:31:55 +0900

    :lipstick: Update style

--- src/OgpMe.js
+++ src/OgpMe.js
@@ -1,39 +1,37 @@
 const template = document.createElement("template");
 template.innerHTML = `
   <style>
-      .card {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        transition: 0.3s;
-        width: 512px;
+      p {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
       }
-      .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+      .card {
+        max-width: 100%;
       }
       .container {
-        padding: 2px 16px;
+        width: 100%;
         background-color: #f2f3f5;
       }
-      .img {
+      .container .img {
         width: 100%;
-        height: auto;
       }
-      .site {
+      .container .site {
         color: #606770;
         font-size: 12px;
         text-transform: uppercase;
       }
-      .title {
+      .container .title {
         font-weight: 600;
       }
-      .description {
+      .container .description {
         color: #606770;
         font-size: 14px;
-        line-height: 20px;
       }
   </style>
   <div class="card">
-    <a class="link" href="#" target="_blank"><img class="img" /></a>
     <div class="container">
+      <a class="link" href="#" target="_blank"><img class="img" /></a>
       <p class="site"></p>
       <p class="title"></p>
       <p class="description"></p>
@@ -41,40 +39,40 @@ template.innerHTML = `
   </div>
   `;
 class OgpMe extends HTMLElement {
+  proxy = "";
+  src = "";
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
   connectedCallback() {
+    this.proxy = this.getAttribute("proxy");
+    this.src = this.getAttribute("src");
     this._render();
   }
-  _render() {
-    const proxy = this.getAttribute("proxy");
-    const src = this.getAttribute("src");
-    fetch(`${proxy}/${src}`).then((res) => {
-      res.text().then((txt) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(txt, "text/html");
-        const image = doc
-          .querySelector("meta[property='og:image']")
-          .getAttribute("content");
-        const site = doc
-          .querySelector("meta[property='og:site_name']")
-          .getAttribute("content");
-        const title = doc
-          .querySelector("meta[property='og:title']")
-          .getAttribute("content");
-        const description = doc
-          .querySelector("meta[property='og:description']")
-          .getAttribute("content");
-        this.shadowRoot.querySelector(".img").setAttribute("src", image);
-        this.shadowRoot.querySelector(".site").innerHTML = site;
-        this.shadowRoot.querySelector(".title").innerHTML = title;
-        this.shadowRoot.querySelector(".description").innerHTML = description;
-        this.shadowRoot.querySelector(".link").setAttribute("href", src);
-      });
-    });
+  async _render() {
+    const txt = await (await fetch(`${this.proxy}/${this.src}`)).text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(txt, "text/html");
+    const image = doc
+      .querySelector("meta[property='og:image']")
+      .getAttribute("content");
+    const site = doc
+      .querySelector("meta[property='og:site_name']")
+      .getAttribute("content");
+    const title = doc
+      .querySelector("meta[property='og:title']")
+      .getAttribute("content");
+    const description = doc
+      .querySelector("meta[property='og:description']")
+      .getAttribute("content");
+    this.shadowRoot.querySelector(".img").setAttribute("src", image);
+    this.shadowRoot.querySelector(".site").innerHTML = site;
+    this.shadowRoot.querySelector(".title").innerHTML = title;
+    this.shadowRoot.querySelector(".description").innerHTML = description;
+    this.shadowRoot.querySelector(".link").setAttribute("href", this.src);
   }
 }
 
